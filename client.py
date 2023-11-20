4474c08093ea627724d452e1d5246df34a9830a1
Henry Chadban <HenryJ.Chadban@gmail.com>
2023-11-20 19:25:47 +1100

    moved all code into functions

--- client.py
+++ client.py
@@ -1,8 +1,11 @@
 #this program runs on the remote PC controlling the Raspberry-Pi, this could by running MacOS (which I developed for), Windows, Linux or another Raspberry Pi
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("raspberrypi",5000))
+def connect_server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("raspberrypi",5008))
+    while True:
+        print(s.recv(1024).decode("utf-8"))
 
-while True:
-    print(s.recv(1024).decode("utf-8"))
+if __name__ == '__main__':
+    connect_server()
